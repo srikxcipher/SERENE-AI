bae7dbfdf81ddb8c47cd69949e0356ea64f37782
Jyotiprakash-01 <jyotiprakashtripathy2004@gmail.com>
2024-10-18 18:28:08 +0530

    calling the findbyemail function to check if the user if the user exits or not if not then proceed to create an user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,24 @@
 const User = require('../models/userModel');
-const bcrypt=require('bcrypt');
-const jwt=require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const register = (req,res) =>{
-    const{username,email,password }=req.body;
-    bcrypt.hash(password, 10,(err,hash) =>{
-        if(err)
-            return res.status(500).json({error: 'Error hashing password'});
-        const newUser={username,email,password: hash};
-        User.create(newUser,(erroe, result) =>{
-            if(error) return res.status(500).json({error});
-            res.status(201).json({message: 'User registered successfully'});
+const register = (req, res) => {
+    const { username, email, password } = req.body;
+    //checking if the user exists
+    User.findByEmail(email, (error, users) => {
+        if (error) return res.status(500).json({ error: 'Error checking for the user' });
+        if (users.length > 0) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
+
+        bcrypt.hash(password, 10, (err, hash) => {
+            if (err)
+                return res.status(500).json({ error: 'Error hashing password' });
+            const newUser = { username, email, password: hash };
+            User.create(newUser, (erroe, result) => {
+                if (error) return res.status(500).json({ error });
+                res.status(201).json({ message: 'User registered successfully' });
+            });
         });
     });
 };
