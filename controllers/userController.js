458067a4522fdd26bf8b2e04918fd0160d286960
Jyotiprakash-01 <jyotiprakashtripathy2004@gmail.com>
2024-10-18 18:19:11 +0530

    added a register function in the userController to hash password using bcrypt and create a new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,18 @@
 const User = require('../models/userModel');
 const bcrypt=require('bcrypt');
 const jwt=require('jsonwebtoken');
+
+const register = (req,res) =>{
+    const{username,email,password }=req.body;
+    bcrypt.hash(password, 10,(err,hash) =>{
+        if(err)
+            return res.status(500).json({error: 'Error hashing password'});
+        const newUser={username,email,password: hash};
+        User.create(newUser,(erroe, result) =>{
+            if(error) return res.status(500).json({error});
+            res.status(201).json({message: 'User registered successfully'});
+        });
+    });
+};
+
+
